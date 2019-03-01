11094a3a1a32633c1456204ade71e132b578fe67
zschreibs <47578854+zschreibs@users.noreply.github.com>
2019-02-28 20:51:54 -0500

    Say my name (#3)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Subrat!");
-        System.out.println("Hello subratp!");
+        System.out.println("Hello Zara!");
+        System.out.println("Hello zschreibs!");
     }
 }
