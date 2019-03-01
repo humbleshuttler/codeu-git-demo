4725dc3290899e23c23c051962c149ec7d59322c
katsun14 <katherinesun14@gmail.com>
2019-02-28 20:28:54 -0500

    Katherine

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Zara!");
         System.out.println("Hello zschreibs!");
+        System.out.println("Hello Subrat!");
+        System.out.println("Hello subratp!");
+        System.out.println("Hi Katherine!");
     }
 }
